0eb6979cf2fc1d73aa0bc536e5d3cf3d0cb76c3e
Mathieu Dorville <mtd42@users.noreply.github.com>
2021-02-05 14:11:21 +0100

    Feature/plan (#1)
    * Add solar system
    
    * ajout texture
    
    * Add orbit control
    
    Co-authored-by: Jimmy Boumans <jimmy.boumans@gmail.com>

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const THREE = require('three')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'SolarSystem' });
 });
 
 module.exports = router;
